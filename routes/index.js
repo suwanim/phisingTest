70af63adf16ad6dd55d94dc5c628fc9bbdb85bd6
suwanim <suwan_im@outlook.com>
2025-07-24 17:59:07 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
-const axios = require("axios");
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log('Client IP:', clientIp);
 
-
   res.render("index", {
     title: `Microleasing Healthcare`,
     clientIp: `${clientIp}`,
   });
-
-});
-
-router.post('/submit', async function (req, res, next) {
-  console.log('/submit', req.body)
-
-  // ดึงข้อมูลจากฟอร์ม
-  // ยิงไปยัง API ของอีกโปรเจค
-  const response = await axios.post('https://apidev01.microleasingplc.com:8001/submit', data, {
-    headers: {
-      'Content-Type': 'application/json'
-      // ถ้าปลายทางต้องใช้ token / auth ก็เพิ่ม header ตรงนี้
-    }
-  });
-
-  console.log('✅ ส่งข้อมูลไปปลายทางเรียบร้อย:', response.status);
-
-  // ตอบกลับไปยัง client
-  res.send('<h2>ส่งข้อมูลไป API ปลายทางเรียบร้อยแล้ว!</h2>');
 
 });
 
