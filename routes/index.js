18b9fbef41496be29a0db4789cd457994df20b21
suwanim <suwan_im@outlook.com>
2025-07-24 17:26:06 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,18 @@ router.get('/', async function (req, res, next) {
   const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   console.log('Client IP:', clientIp);
 
-  const response = await axios.post('https://apidev01.microleasingplc.com:8001/loginLog', JSON.stringify(`{"ip-addr1":"${clientIp}", "ip-addr2":"-"},\r\n`), {
-    headers: {
-      'Content-Type': 'application/json'
-      // ถ้าปลายทางต้องใช้ token / auth ก็เพิ่ม header ตรงนี้
-    }
-  });
-
+  // await axios.post('https://apidev01.microleasingplc.com:8001/loginLog', JSON.stringify(`{"ip-addr1":"${clientIp}", "ip-addr2":"-"},\r\n`), {
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //     // ถ้าปลายทางต้องใช้ token / auth ก็เพิ่ม header ตรงนี้
+  //   }
+  // });
 
   res.render("index", {
     title: `Microleasing Healthcare`,
     clientIp: `Client IP: ${clientIp}`,
   });
+
 });
 
 router.post('/submit', async function (req, res, next) {
